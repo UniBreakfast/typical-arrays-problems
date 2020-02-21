cbacb1a8c91f086c38467f8e6f2c032a3813d7ab
unibreakfast <unibreakfast1@gmail.com>
2020-02-21 19:24:58 +0200

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  return arr && arr.length ? Math.min(...arr) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  return arr && arr.length ? Math.max(...arr) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  return arr && arr.length ? arr.reduce((sum, num) => sum + num)/arr.length : 0;
 }
